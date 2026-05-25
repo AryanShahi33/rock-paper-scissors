6a2bf616870a6a8e9a6e95a23a90c4e907ad5633
Aryan <209350547+AryanShahi33@users.noreply.github.com>
2026-05-25 13:13:53 +0545

    loop the game for 5 rounds and fix the bug of human score and computer score looping back to 0 everytime

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Use a random number generator to make the computer pick between rock paper or scissors
 // create the function
+let humanScore = 0;
+let computerScore = 0;
 function getComputerChoice(){
     //create a variable to store the random number
+    let computerChoice;
     let random = Math.floor(Math.random()*3);
-    let computerChoice = "";
     if (random == 0){
-        computerChoice = "rock";
+        computerChoice = "rock";        
     }
     else if (random == 1){
         computerChoice = "paper";
@@ -13,7 +15,6 @@ function getComputerChoice(){
     else {
         computerChoice = "scissors";
     }
-    console.log(computerChoice);
     return computerChoice;
 }
 //create a function to get input from user for rock paper or scissors
@@ -26,64 +27,69 @@ function getHumanChoice(){
     //return the value 
     return userChoice;
 }
+
+// loop playGame to make the game 5 rounds
+function playGame(){
+
+    for (let i = 1;i<=5;i++)
+    {   
+        // repeat the process of asking 5 different inputs for both human and randomize for the computer
+        let user = getHumanChoice();
+        let computer = getComputerChoice();
+        playRound(user,computer);
+        console.log(user, computer);
+    }
+    if (humanScore>computerScore){
+        console.log("Human wins");
+    }
+    else if (computerScore>humanScore) {
+        console.log("Computer wins");
+    }
+    else {
+        console.log("Draw");
+    }
+}
+
 // Developing logic for the actual game
 // First get the input of both user and computer in the function
 function playRound(humanChoice,computerChoice){
-    let humanScore = 0;
-    let computerScore = 0;
+
+
     // Use comparsion operators to determine the winner
     if (humanChoice == "rock" && computerChoice == "scissors")
     {
         // Print out the winner
-        console.log("rock Beats scissors")
+        console.log("You win!,rock Beats scissors")
         // Increment the score of the winner
         humanScore++;
     }
     else if (humanChoice == "scissors" && computerChoice == "paper")
     {
-        console.log("scissors beats paper")
         humanScore++;
+        console.log("You win!, scissors beats paper")
     }
     else if (humanChoice == "paper" && computerChoice == "rock")
     {
-        console.log("paper beats rock")
         humanScore++;
+        console.log("You win!, paper beats rock")
     }
     else if (humanChoice == "rock" && computerChoice == "paper")
     {
-        console.log("paper beats rock")
         computerScore++;
+        console.log("You lose!, paper beats rock")
     }
     else if (humanChoice == "scissors" && computerChoice == "rock")
     {
-        console.log("rock beats scissors")
         computerScore++;
+        console.log("You lose!, rock beats scissors")
     }
     else if (humanChoice == "paper" && computerChoice == "scissors")
     {
-        console.log("scissors beats paper")
         computerScore++;
+        console.log("You lose!, scissors beats paper");
     }
-    console.log(computerScore,humanScore);
     return computerScore,humanScore;
 }
 
-// loop playGame to make the game 5 rounds
-function playGame(humanScore,computerScore){
-    for (let i = 1;i<5;i++)
-    {   
-        let user = getHumanChoice();
-        let computer = getComputerChoice();
-        playRound(user,computer);
-        console.log(user,computer);
-    }
-    if (humanScore>computerScore){
-        console.log("Human wins by "+ humanScore-computerScore + "Score");
-    }
-    else {
-        console.log("Computer wins by "+ computerScore-humanScore + "Score");
-    }
-}
-let computerScore = playRound();
-let humanScore = playRound();
-playGame(humanScore,computerScore);
+
+playGame();
