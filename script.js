62bf0a7da9e21cf5b0d3453a5e6012f7ef83684d
Aryan <209350547+AryanShahi33@users.noreply.github.com>
2026-05-25 09:20:51 +0545

    Add User Input choice

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Use a random number generator to make the computer pick between rock paper or scissors
 // create the function
 function getComputerChoice(){
+    //create a variable to store the random number
     let random = Math.floor(Math.random()*3);
     let computerChoice = "";
     if (random == 0){
@@ -15,7 +16,18 @@ function getComputerChoice(){
     console.log(computerChoice);
     return computerChoice;
 }
-//create a variable to store the random number
+//create a function to get input from user for rock paper or scissors
+function getHumanChoice(){
+    //ask user for input
+    let userChoice = prompt("Choose between rock paper and scissors");
+    //convert user input to uppercase to avoid any issues
+    let uppercase = userChoice.toUpperCase();
+    userChoice = uppercase;
+    //return the value 
+    return userChoice;
+}
+// get the return value and store it in a glboal variable
 let computer = getComputerChoice();
-
+let user  = getHumanChoice();
+console.log(computer,user);
 
